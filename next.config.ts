9a565e15a977f016583b9899254f8accda28dc30
Igor Sotsugov <234929+sotsugov@users.noreply.github.com>
2024-10-23 22:46:45 +0100

    Use domain to env var

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     const baseUrl =
       process.env.NODE_ENV === 'development'
         ? 'http://localhost:8000/'
-        : '/api';
+        : process.env.NEXT_PUBLIC_API_URL;
 
     return [
       {
